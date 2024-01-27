b92d78573077ce4afdde34ea823ff22b4b8f54c9
Andrija Stanisic <andrija.d.stanisic@gmail.com>
2024-01-27 20:40:05 +0100

    added new examples

--- code/src/main.rs
+++ code/src/main.rs
@@ -3,28 +3,36 @@ mod util;
 use image::{ImageBuffer, Rgb, RgbImage};
 
 fn main() {
-    let image = util::load_image("../images/example_2/main.jpg")
-        .expect("Error occured while loading main image!");
-
+    let image_path = "../images/example_6/main.png";
+    let parts_path = "path/to/directory/";
+    let extension = "jpg";
     let mut parts: Vec<RgbImage> = Vec::new();
 
+    let image = util::load_image(image_path).expect("Error occured while loading main image!");
+    let num_parts = util::count_parts(parts_path, extension);
+    util::load_parts(&mut parts, parts_path, num_parts, extension);
     // EXAMPLE 1
-
     //util::load_parts_1_1(&mut parts, "../images/example_1/parts_1/");
     //util::load_parts_1_2(&mut parts, "../images/example_1/parts_2/");
 
-    //EXAMPLE 2
+    // EXAMPLE 2
+    //util::load_parts_2_1(&mut parts, "../images/example_2/parts_1/");
 
-    util::load_parts_2_1(&mut parts, "../images/example_2/parts_1/");
+    // EXAMPLE 3
+    util::load_parts_1_1(&mut parts, "../images/example_1/parts_1/");
+    // EXAMPLE 4
+
+    // EXAMPLE 5
+
+    // EXAMPLE 6
+
+    util::load_parts_6_1(&mut parts, "../images/example_6/parts_1/");
 
     let image_pix: Vec<Vec<(u8, u8, u8)>> = util::image_to_pixel_matrix(&image);
-    //util::print_matrix_portion(&image_pix, 4);  //debug function
-
     let mut output: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(image.width(), image.height());
     for part in parts.iter() {
         let part_pix = util::image_to_pixel_matrix(&part);
         let (best_x, best_y) = search::find_best_position(&image_pix, &part_pix);
-        println!("({},{})", best_x, best_y);
         util::generate_output(&mut output, &part, (best_x as u32, best_y as u32));
     }
 
--- code/src/util.rs
+++ code/src/util.rs
@@ -1,9 +1,40 @@
 extern crate image;
 use image::{open, ImageBuffer, ImageError, Rgb, RgbImage};
+use std::fs;
+use std::path::Path;
 
 pub fn load_image(path: &str) -> Result<RgbImage, ImageError> {
     let img = open(path)?;
     Ok(img.to_rgb8())
+}
+
+pub fn load_parts(parts: &mut Vec<RgbImage>, path: &str, num_parts: usize, extension: &str) {
+    for i in 0..num_parts {
+        let path = format!("{}image{}.{}", path, i, extension);
+        let error = format!("Error occurred while loading image{}.{}", i, extension);
+        let part = load_image(&path).expect(&error);
+        parts.push(part);
+    }
+}
+
+pub fn count_parts(path: &str, extension: &str) -> usize {
+    let dir = Path::new(path);
+    let mut count = 0;
+
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir).expect("Directory not found") {
+            let entry = entry.expect("Error reading entry");
+            let file_path = entry.path();
+
+            if file_path.is_file() && file_path.extension().unwrap_or_default() == extension {
+                count += 1;
+            }
+        }
+    } else {
+        println!("Provided path is not a directory");
+    }
+
+    count
 }
 
 // EXAMPLE 1
@@ -27,12 +58,50 @@ pub fn load_parts_1_2(parts: &mut Vec<RgbImage>, path: &str) {
 }
 
 // EXAMPLE 2
-
 pub fn load_parts_2_1(parts: &mut Vec<RgbImage>, path: &str) {
     for i in 1..16 {
         let path = format!("{}part_{}.png", path, i);
         let error = format!("Error occurred while loading part_{}.png", i);
         let part = load_image(&path).expect(&error);
+        parts.push(part);
+    }
+}
+
+// EXAMPLE 3
+pub fn load_parts_3(parts: &mut Vec<RgbImage>, path: &str) {
+    for i in 0..191 {
+        let path = format!("{}image{}.jpg", path, i);
+        let error = format!("Error occurred while loading part_{}.png", i);
+        let part = load_image(&path).expect(&error);
+        parts.push(part);
+    }
+}
+
+// EXAMPLE 4
+pub fn load_parts_4(parts: &mut Vec<RgbImage>, path: &str) {
+    for i in 0..14 {
+        let path = format!("{}image{}.png", path, i);
+        let error = format!("Error occurred while loading part_{}.png", i);
+        let part = load_image(&path).expect(&error);
+        parts.push(part);
+    }
+}
+
+// EXAMPLE 5
+pub fn load_parts_5(parts: &mut Vec<RgbImage>, path: &str) {
+    for i in 0..95 {
+        let path = format!("{}image{}.jpg", path, i);
+        let error = format!("Error occurred while loading part_{}.png", i);
+        let part = load_image(&path).expect(&error);
+        parts.push(part);
+    }
+}
+
+// EXAMPLE 6
+pub fn load_parts_6_1(parts: &mut Vec<RgbImage>, path: &str) {
+    for i in 0..=999 {
+        let path = format!("{}image{}.png", path, i);
+        let part = load_image(&path).expect("ERROR");
         parts.push(part);
     }
 }
@@ -52,15 +121,6 @@ pub fn image_to_pixel_matrix(img: &RgbImage) -> Vec<Vec<(u8, u8, u8)>> {
     matrix
 }
 
-pub fn print_matrix_portion(matrix: &Vec<Vec<(u8, u8, u8)>>, size: usize) {
-    for row in matrix.iter().take(size) {
-        for &pixel in row.iter().take(size) {
-            print!("{:?} ", pixel);
-        }
-        println!();
-    }
-}
-
 pub fn generate_output(
     assembled: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
     part: &ImageBuffer<Rgb<u8>, Vec<u8>>,
@@ -76,3 +136,13 @@ pub fn generate_output(
         }
     }
 }
+
+// Debug function
+// pub fn print_matrix_portion(matrix: &Vec<Vec<(u8, u8, u8)>>, size: usize) {
+//     for row in matrix.iter().take(size) {
+//         for &pixel in row.iter().take(size) {
+//             print!("{:?} ", pixel);
+//         }
+//         println!();
+//     }
+// }
